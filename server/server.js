a2904875f132d8d00903f400d0b0e204c073394e
febriadj <iamfebriadji@gmail.com>
2021-07-20 15:52:49 +0700

    update server.js

---
+++ server/server.js
@@ -0,0 +1,27 @@
+const express = require('express');
+const path = require('path');
+
+const app = express();
+const http = require('http').createServer(app);
+const io = require('socket.io')(http);
+
+const port = process.env.PORT ?? 3000;
+
+app.set('views', 'client/views');
+app.set('view engine', 'pug');
+app.use(express.static(path.join(__dirname, '..', 'client')));
+
+app.get('/', (req, res) => res.render('index'));
+
+io.on('connection', (socket) => {
+  socket.on('new_message', (args) => {
+    const { username, time, message } = args;
+
+    io.emit('new_message', {
+      username, time, message,
+    });
+  });
+});
+
+http.listen(port);
+console.log(`app running on port ${port}`);
