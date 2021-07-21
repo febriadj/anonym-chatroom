146be3889c1bf0dcc21a71e58fc6849e88abc298
febriadj <iamfebriadji@gmail.com>
2021-07-21 15:53:07 +0700

    update javascript client

--- client/javascript/header.js
+++ client/javascript/header.js
@@ -1,9 +1,25 @@
 const userOnline = document.getElementsByClassName('users_online');
 
 socket.on('userConnected', ({ online }) => {
-  userOnline[0].innerHTML = `${online} Online`;
+  userOnline[0].innerHTML = `${online} pengguna sedang Online`;
 });
 
 socket.on('userDisconnect', (users) => {
-  userOnline[0].innerHTML = `${users} Online`;
+  userOnline[0].innerHTML = `${users} pengguna sedang Online`;
+});
+
+const headerUsername = document.getElementsByClassName('header_username');
+const logoutBtn = document.getElementsByClassName('logout_btn');
+
+function showNameOnHeader() {
+  const username = localStorage.getItem('username');
+  headerUsername[0].innerHTML = username;
+}
+
+showNameOnHeader();
+
+logoutBtn[0].addEventListener('click', () => {
+  localStorage.removeItem('username');
+
+  window.location.reload(true);
 });
--- client/javascript/screen.js
+++ client/javascript/screen.js
@@ -1,9 +1,12 @@
 const screenWrap = document.getElementsByClassName('screen_wrap');
 
 function chatBoxElement(args) {
+  const ifYou = localStorage.getItem('username');
+  const boxColorIfItsYou = ifYou === args.username ? '#ffe8d6' : '#f6f9fd';
+
   const element = `<div class="chat_box">
     <span class="chat_dot"></span>
-    <div class="chat_msg">
+    <div class="chat_msg" style="background: ${boxColorIfItsYou}">
       <p class="chat_username">${args.username}</p>
       <p class="msg">${args.message}</p>
       <p class="chat_time">${args.time}</p>
@@ -11,6 +14,7 @@ function chatBoxElement(args) {
   </div>`;
 
   screenWrap[0].innerHTML += element;
+  screenWrap[0].scrollIntoView(false);
 }
 
 socket.on('showNewMessage', (args) => {
